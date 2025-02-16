33975eb58c6b492e091a49eb52be033d8c7b61bd
Leandro Martins <leandro2828@gmail.com>
2025-02-16 09:01:25 -0300

    Criado User e Order e conigurado relacionamento

---
+++ src/main/java/com/devsuperior/dscomerce1/entities/Order.java
@@ -0,0 +1,75 @@
+package com.devsuperior.dscomerce1.entities;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant moment;
+	private OrderStatus orderStatus;
+	
+	@ManyToOne
+	@JoinColumn(name = "client_id") // Define explicitamente o nome da coluna na tabela
+	private User client;
+	
+	public Order() {
+	}
+	
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.orderStatus = orderStatus;
+		this.client = client;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/devsuperior/dscomerce1/entities/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dscomerce1.entities;
+
+public enum OrderStatus {
+	
+	WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+
+}
