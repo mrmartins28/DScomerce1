7c83ec1a1fba995c46c3859efa5caa35ca644d58
Leandro Martins <leandro2828@gmail.com>
2025-02-18 19:04:48 -0300

    getProducts e getOrders

--- src/main/java/com/devsuperior/dscomerce1/entities/Order.java
+++ src/main/java/com/devsuperior/dscomerce1/entities/Order.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscomerce1.entities;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
@@ -88,6 +89,15 @@ public class Order {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
+	public List<Product> getProducts() {
+		return items.stream().map(x -> x.getProduct()).toList();
+	}
+	
 	
 
 }
--- src/main/java/com/devsuperior/dscomerce1/entities/Product.java
+++ src/main/java/com/devsuperior/dscomerce1/entities/Product.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscomerce1.entities;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Column;
@@ -91,6 +92,14 @@ public class Product {
 	
 	public Set<Category> getCategories() {
 		return categories;
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
+	public List<Order>	getOrders(){
+		return items.stream().map(x -> x.getOrder()).toList();
 	}
 
 }
